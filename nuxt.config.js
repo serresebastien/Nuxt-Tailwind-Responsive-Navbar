d9f1d13a7f97eb8afba4128d20940554ea40d70f
serresebastien <serre_sebastien@hotmail.fr>
2020-10-25 23:00:15 +0100

    Title change

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'nuxt-tailwind-responsive-navbar',
+    title: 'Responsive Navbar | Nuxt & Tailwind',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
